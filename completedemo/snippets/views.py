2eda378e78359e85280233e193c20477ee612c14
hilarionq <hilarionq@gmail.com>
2017-08-04 16:38:29 -0400

    Adding Object level permissions.

--- completedemo/snippets/views.py
+++ completedemo/snippets/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -17,7 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
