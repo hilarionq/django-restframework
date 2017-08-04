2b0d26bac58601adb3159f1ac72df2dc61d5bc18
hilarionq <hilarionq@gmail.com>
2017-08-04 16:17:09 -0400

    Adding login to the Browsable API. also fixing some typos on views.

--- completedemo/snippets/views.py
+++ completedemo/snippets/views.py
@@ -9,7 +9,7 @@ from snippets.serializers import UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -17,7 +17,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
