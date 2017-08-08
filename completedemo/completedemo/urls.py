92bd77bae259feaa30f8c2a2c2c06d9637cf2785
hilarionq <hilarionq@gmail.com>
2017-08-08 18:56:46 -0400

    Adding a schema.

--- completedemo/completedemo/urls.py
+++ completedemo/completedemo/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
-    url(r'^', include('snippets.urls')),
+    url(r'^schema/$', schema_view),
+    url(r'^', include('snippets.urls'))
 ]
